140e22cfd15b6ef73b264f69bc24d363e4c5f43a
veenNgugi <ngugivee@gmail.com>
2026-03-18 19:17:28 +0300

    Experience section

---
+++ pages/experience.php
@@ -0,0 +1,14 @@
+<?php
+$pageTitle = 'Projects | Dan Portfolio';
+require __DIR__ . '/../partials/header.php';
+?>
+<!-- EXPERIENCE -->
+<section id="experience" class="section snap-section">
+    <h3 class="section-title">Experience</h3>
+    <div class="experience-track" id="experience-track">
+        <!-- Injected by JS -->
+    </div>
+</section>
+
+
+<?php require __DIR__ . '/../partials/footer.php'; ?>
